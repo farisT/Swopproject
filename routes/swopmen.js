de45689b2eaaf6f8055d157d313290bee4963bc3
Patricia Schutter <patricia_schutter@hotmail.com>
2017-12-22 16:39:35 +0100

    overview page layout set

--- routes/swopmen.js
+++ routes/swopmen.js
@@ -23,5 +23,34 @@ module.exports = (app, db) => {
 			console.log(e)
 		})
 	})
+
+	// ROUTE FOR  THE CATEGORIESSS DONT KNOW HOW TO SOLVE IT NICELY YET
+	// keep in mind there is a little bit of jquery in pug page to change the selected element 
+	app.get('/swopmenSuits', (req, res) => { 
+		db.clothes.findAll({
+			where: {
+			gender: "Men",
+			category: "suits"
+			}
+		})
+		.then((result)=> {
+			console.log(result)
+			if (req.session.user){
+				res.render("swopmen", {
+					overviewArray: result,
+					first_name: req.session.user.first_name
+				})
+			}
+			else{
+				res.render("swopmen", {
+					overviewArray: result
+				})
+			}
+		})
+		.catch((e) => {
+			console.log(e)
+		})
+	})
+
 }
 
