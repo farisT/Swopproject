37fc8a8e6efe477848ffc1dd729a540b3c39a6fa
Faris Tangestani <tangestanifaris@gmail.com>
2017-12-22 11:19:45 +0100

    select all clothes db

--- routes/swopmen.js
+++ routes/swopmen.js
@@ -1,9 +1,26 @@
 module.exports = (app, db) => {
 	app.get('/swopmen', (req, res) => {
 		if(req.session.user){
-			res.render('swopmen', {
-				first_name: req.session.user.first_name
-			})
+
+		db.clothes.findAll({
+			where: {
+			gender: "Men"
+			}
+		})
+		.then((result)=> {
+
+			console.log(result)
+		})
+		.catch((e) => {
+			console.log(e)
+		})
+
+
+		res.send("it worked!")
+
+			// res.render('swopmen', {
+			// 	first_name: req.session.user.first_name
+			// })
 		}
 		else {
 			res.render('swopmen')
--- routes/uploadpage.js
+++ routes/uploadpage.js
@@ -29,7 +29,19 @@ module.exports = (app, db, upload, path, fs) => {
 				comments: `${req.body.comments}`,
 				condition: `${req.body.buttonCondition}`,
 			})
-		res.send("your item has been uploaded!")
+		res.render("profile" , {
+			id: req.session.user.id,
+			first_name: req.session.user.first_name,
+			last_name: req.session.user.last_name,
+			email: req.session.user.email,
+			subscription: req.session.subscription,
+			address: req.session.user.address,
+			zip_code: req.session.user.zip_code,
+			city: req.session.user.city,
+			phone_number: req.session.user.phone_number,
+			date_of_birth: req.session.user.date_of_birth,
+			hashedPassword: req.session.user.hashedPassword,
+		})
 	})
 }
 
