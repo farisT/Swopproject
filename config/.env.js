4e208a312283cb49301529f7fec8f633c97b75a2
Faris Tangastani <ftangastani@gmail.com>
2018-05-28 12:57:20 +0200

    pre heroku push

---
+++ config/.env.js
@@ -0,0 +1,10 @@
+const env = {
+ PORT: 5432, 
+ DATABASE_NAME: 'swop',
+ DATABASE_HOST: 'localhost',
+ DATABASE_USERNAME: 'postgres',
+ DATABASE_PASSWORD: 'biggles',
+ DATABASE_DIALECT: 'postgres',
+ WEBPORT: 3000
+}
+module.exports = env 
